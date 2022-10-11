20ffc99732f6e67fa04453ec632aa7a8c454df79
Özgür Öğdem <ogdemster@gmail.com>
2022-10-11 12:56:08 +0300

    Github Pages Complated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
